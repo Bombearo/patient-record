dbcda68a44452e59188a62b38eeca07c9073f3e5
Bombearo <shekjaden@gmail.com>
2025-06-08 15:51:40 +0100

    Displayed all appointments

--- src/app/patient/[slug]/page.tsx
+++ src/app/patient/[slug]/page.tsx
@@ -2,16 +2,15 @@
 import patients from "@/data/patients"
 import appointments from "@/data/appointments"
 import {Patient} from "@/types/patient"
-import {useEffect, useState} from "react"
+import { Appointment } from "@/types/appointment"
+import {useEffect, useState, use} from "react"
 
 interface PatientPageProps{
-    params:{
-        slug:string;
-    }
+    params:Promise<{slug:string}>
 }
 
 function getPatient(patientID:string){
-    const possiblePeople:Patient[] = patients.filter((person) => person.patientID === patientID)
+    const possiblePeople:Patient[] = patients.filter((person) => {return person.patientID === patientID})
     if (possiblePeople.length === 0){
         return null;
     }
@@ -19,14 +18,44 @@ function getPatient(patientID:string){
 
 }
 
+function getAppointments(patientID:string){
+    const patientAppointments:Appointment[] = appointments.filter((appointment) =>{
+        return appointment.patientID === patientID
+    })
+    return patientAppointments;
+}
 
-function PatientPage({params: {slug}}:PatientPageProps) {
+
+function PatientPage({params}:PatientPageProps) {
+    const slug = use(params)
     const [currentPatient,setCurrentPatient] = useState<Patient|null>(null)
+    const [patientAppointments, setPatientAppointments] = useState<Appointment[]|null>(null)
 
     useEffect(() =>{
-        const patient:Patient|null = getPatient(slug)
+        
+        const id:string = slug.slug
+
+        const patient:Patient|null = getPatient(id)
         setCurrentPatient(patient)
+        console.log(currentPatient)
+        
     }, [])
+
+
+    // Update the appointment state when currentPatient exists
+    useEffect(() =>{
+        console.log(currentPatient)
+        if (currentPatient !== null){
+
+            const apps = getAppointments(currentPatient.patientID)
+            console.log(apps)
+            setPatientAppointments(apps)
+        }
+
+    }, [currentPatient]
+    
+    )
+    
 
 
     if (currentPatient == null){
@@ -34,7 +63,15 @@ function PatientPage({params: {slug}}:PatientPageProps) {
     }
     return (<div>
         {currentPatient.name}
+        <div>
+            {patientAppointments && patientAppointments.map((app,index) => (
+                <div key={index}>
+                    {app.id}
+                    {app.status}
 
+                </div>
+            ))}
+        </div>
     </div>  );
 }
 
